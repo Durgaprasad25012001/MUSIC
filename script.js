52e7f4a11dc460024df1961c4020b4c5065efb3c
MAHANTI DURGAPRASAD <durgamahanthi0123@gmail.com>
2025-04-03 07:34:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+
+function function1() {
+    var x= document.getElementById('song')
+    x.src = "https://www.dropbox.com/scl/fi/dou8s5d79lhkpzawrg2a9/Hey-Minnale.mp3?rlkey=14t20kqgenrx9wn4qnr2ajerl&st=w5endbv8&raw=1";
+    x.play();
+ }
+ function function2() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/hi2hqauzbtsootdswxuep/Kurchi-Madathapetti.mp3?rlkey=nuiwbykz7ujzgijink6piv636&st=qwcjfezo&raw=1";
+     x.play();
+  }
+  function function3() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/dlnihrxl1jcxn66iugcmz/Godari-Gattu.mp3?rlkey=kgg1zolcczkfjdipe60eooz92&st=2y6oaa7b&raw=1";
+     x.play();
+  }
+  function function4() {
+     var x= document.getElementById('song')
+        x.src = "https://www.dropbox.com/scl/fi/xswhzrc3lzfb741r2lsw2/Premalo.mp3?rlkey=bnikl4bc9kxpyw4tylv67bcri&st=9eo2mdib&raw=1";
+        x.play();
+  }
+  function function5() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/jxhvpcifdl44lxkzhryw8/Hilesso-Hilessa.mp3?rlkey=q73gfoj8mh4rg04up1exx2iib&st=jx8cd06m&raw=1";
+     x.play();
+  }
+  function function6() {
+     var x= document.getElementById('song')
+     x.src = "";
+     x.play();
+  }
+  function function7() {
+     var x= document.getElementById('song')
+     x.src = "";
+     x.play();
+  }
+  function function8() {
+     var x= document.getElementById('song')
+     x.src = "";
+     x.play();
+  }
+  function function9() {
+     var x= document.getElementById('song')
+     x.src = "";
+     x.play();
+  }
+  function function10() {
+     var x= document.getElementById('song')
+     x.src = "";
+     x.play();
+  }
+  function function11() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/eiqml5o0w5zdymkn8llcb/Arabic-Kuthu.mp3?rlkey=vwlhkyxtx0lz40wh5kkjcuock&st=vjxnkrlv&raw=1";
+     x.play();
+  }
+  function function12() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/yjxx5b3ngbzx16ab6b4wv/Dum-Masala.mp3?rlkey=nb836daq5ewaxoblbhg9vg181&st=y2g6iwmy&raw=1";
+     x.play();
+  }
+  function function13() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/jqhrmajguiprnu0w2p6g3/Private-Party-SenSongsMp3.Co.mp3?rlkey=pa4d5tiywbhu7m7hrrqznkzx2&st=cnsouo4n&raw=1";
+     x.play();
+  }
+  function function14() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/f577o01610yydmdujkhys/Nattu-Nattu.mp3?rlkey=wzv4ln74rf3koc26ri111fpg0&st=n35c6bsq&raw=1";
+     x.play();
+  }
+  function function15() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/n1lg58qkzx19exbsojqmg/24by7-Party-SenSongsmp3.Co.mp3?rlkey=3o525gd52bpmi2b4tvqbhjhnm&st=2vtanxmm&raw=1";
+     x.play();
+  }
+  function function16() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/7gw6orwla36sr5aanqdt1/Bharatha-Vedamuga-SenSongsMp3.Co.mp3?rlkey=oo2bn4wd58vor9l9q6lngxbyl&st=02q0nvrj&raw=1";
+     x.play();
+  }
+  function function17() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/x30y72ys9c8iai9lmcrfv/05-Madhura-Madhuratara-SenSongsMp3.co.mp3?rlkey=4tbg6doqmk1gm8g6q0oe21cra&st=8mevepcj&raw=1";
+     x.play();
+  }
+  function function18() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/y4wlg9io1pndsf118wy1p/3-Akhilanda-Koti-SenSongsMp3.Co.mp3?rlkey=j80f59w38yoec6dxhlu1zm08s&st=mrz1ghwt&raw=1";
+     x.play();
+  }
+  function function19() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/2a7qom3819a1pw05mv5j6/Jai-Shriram.mp3?rlkey=g122sv0dpzb1wxjurkjjpl2a9&st=aaba3g0y&raw=1";
+     x.play();
+  }
+  function function20() {
+     var x= document.getElementById('song')
+     x.src = "https://www.dropbox.com/scl/fi/rh7gqptglfknranh25wey/Ayyappa-Devaya-SenSongsMp3.co.mp3?rlkey=sq020uu9n9zitnzye2sr66bq9&st=bxwci993&raw=1";
+     x.play();
+  }
+  function function21() {
+   var x= document.getElementById('song')
+   x.src = "https://www.dropbox.com/scl/fi/u4qtu51frwj7bjtswu1fp/Jabilli-Kosam-Male-SenSongsMp3.Co.mp3?rlkey=0olnmxuqxeol3mirix0wbsyxa&raw=1";
+   x.play();
+}
+function function22() {
+   var x= document.getElementById('song')
+   x.src = "https://www.dropbox.com/scl/fi/l22nd16rpzwc9b4ve687h/01-Kannanule-SenSongsMp3.co.mp3?rlkey=c9xofhwsvzd1it37fb4unlt23&raw=1";
+   x.play();
+}
+function function23() {
+   var x= document.getElementById('song')
+   x.src = "https://www.dropbox.com/scl/fi/x56adczrub7w5dpwn2x1s/02-Jaamu-Rathiri-SenSongsMp3.co.mp3?rlkey=4f79gzxttqlsm13u4ncdu7qk5&raw=1";
+   x.play();
+} 
+function function24() {
+   var x= document.getElementById('song')
+   x.src = "https://www.dropbox.com/scl/fi/4h1t2phd3ldtixjrti61e/04-Paruvam-Vanaga-SenSongsMp3.Co.mp3?rlkey=a53k2docqul27ii4j9kti8cco&raw=1";
+   x.play();
+}
+function function25() {
+   var x= document.getElementById('song')
+   x.src = "https://www.dropbox.com/scl/fi/8tqx4ffej2522h24rbeak/Prema-Ane-SenSongsMp3.Co.mp3?rlkey=1yqqd432rvdg0a6ez1k1zoafu&raw=1";
+   x.play();
+}
+ 
